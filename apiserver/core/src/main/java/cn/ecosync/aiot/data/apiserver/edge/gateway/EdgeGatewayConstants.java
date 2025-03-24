9087214c20d85e60c69d10dcd982c8df77befc14
yuenzai <80330775@qq.com>
2025-03-24 16:46:14 +0800

    refactor: Add response-header 'X-Prometheus-Remote-Write-*-Written'

--- apiserver/core/src/main/java/cn/ecosync/aiot/data/apiserver/edge/gateway/EdgeGatewayConstants.java
+++ apiserver/core/src/main/java/cn/ecosync/aiot/data/apiserver/edge/gateway/EdgeGatewayConstants.java
@@ -2,4 +2,7 @@ package cn.ecosync.aiot.data.apiserver.edge.gateway;
 
 public class EdgeGatewayConstants {
     public static final String TOPIC_AIOT_EDGE_GATEWAY_PROMETHEUS = "aiot-edge-gateway-prometheus";
+    public static final String X_PROMETHEUS_REMOTE_WRITE_SAMPLES_WRITTEN = "X-Prometheus-Remote-Write-Samples-Written";
+    public static final String X_PROMETHEUS_REMOTE_WRITE_HISTOGRAMS_WRITTEN = "X-Prometheus-Remote-Write-Histograms-Written";
+    public static final String X_PROMETHEUS_REMOTE_WRITE_EXEMPLARS_WRITTEN = "X-Prometheus-Remote-Write-Exemplars-Written";
 }
--- apiserver/core/src/main/java/cn/ecosync/aiot/data/apiserver/edge/gateway/EdgeGatewayRestController.java
+++ apiserver/core/src/main/java/cn/ecosync/aiot/data/apiserver/edge/gateway/EdgeGatewayRestController.java
@@ -1,10 +1,14 @@
 package cn.ecosync.aiot.data.apiserver.edge.gateway;
 
+import cn.ecosync.aiot.data.apiserver.api.prometheus.Request;
+import cn.ecosync.aiot.data.apiserver.api.prometheus.TimeSeries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,8 +19,9 @@ import org.springframework.web.context.request.async.DeferredResult;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.function.BiConsumer;
 
-import static cn.ecosync.aiot.data.apiserver.edge.gateway.EdgeGatewayConstants.TOPIC_AIOT_EDGE_GATEWAY_PROMETHEUS;
+import static cn.ecosync.aiot.data.apiserver.edge.gateway.EdgeGatewayConstants.*;
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
@@ -41,6 +46,8 @@ public class EdgeGatewayRestController {
             return deferredResult;
         }
 
+        log.atInfo().addKeyValue("gatewayCode", gatewayCode).log("Prometheus remote-write data received from edge-gateway");
+
         String contentEncoding = requestHeaders.getFirst("Content-Encoding");
         if (!"snappy".equals(contentEncoding)) {
             log.atError().addKeyValue("contentEncoding", contentEncoding).log("Unknown Content-Encoding, only 'snappy' supported");
@@ -56,25 +63,22 @@ public class EdgeGatewayRestController {
             return deferredResult;
         }
 
-        log.atInfo().addKeyValue("gatewayCode", gatewayCode).log("Prometheus remote-write data received from edge-gateway");
-
         try {
             byte[] bytes = toByteArray(inputStream);
-//            Request request = PrometheusRequestDecoder.toRequest(bytes);
+            Request request = PrometheusRequestDecoder.toRequest(bytes);
+            MultiValueMap<String, String> responseHeaders = new LinkedMultiValueMap<>(1);
+            handle(request, (responseHeader, writtenCount) -> responseHeaders.set(responseHeader, writtenCount.toString()));
+            // Send to kafka
             kafkaTemplate.send(TOPIC_AIOT_EDGE_GATEWAY_PROMETHEUS, gatewayCode, bytes)
                     .whenComplete((result, ex) -> {
                         if (ex == null) {
-//                    int sampleCount = PrometheusRequestDecoder.getSampleCount(request);
-//                    MultiValueMap<String, String> responseHeaders = new LinkedMultiValueMap<>(1);
-//                    responseHeaders.set("X-Prometheus-Remote-Write-Samples-Written", String.valueOf(sampleCount));
-//                    deferredResult.setResult(new ResponseEntity<>(responseHeaders, NO_CONTENT));
-                            deferredResult.setResult(new ResponseEntity<>(NO_CONTENT));
+                            deferredResult.setResult(new ResponseEntity<>(responseHeaders, NO_CONTENT));
                         } else {
                             log.error("", ex);
                             deferredResult.setErrorResult(new ResponseEntity<>(INTERNAL_SERVER_ERROR));
                         }
                     });
-        } catch (Exception e) {
+        } catch (IOException e) {
             log.error("", e);
             deferredResult.setErrorResult(new ResponseEntity<>(BAD_REQUEST));
         }
@@ -90,5 +94,14 @@ public class EdgeGatewayRestController {
         }
         buffer.flush();
         return buffer.toByteArray();
+    }
+
+    private static void handle(Request request, BiConsumer<String, Integer> written) {
+        int sampleCount = request.getTimeseriesList().stream()
+                .mapToInt(TimeSeries::getSamplesCount)
+                .reduce(0, Integer::sum);
+        written.accept(X_PROMETHEUS_REMOTE_WRITE_SAMPLES_WRITTEN, sampleCount);
+        written.accept(X_PROMETHEUS_REMOTE_WRITE_HISTOGRAMS_WRITTEN, 0);
+        written.accept(X_PROMETHEUS_REMOTE_WRITE_EXEMPLARS_WRITTEN, 0);
     }
 }
