9a4a60b76889092f440e685e277adde80a3663b0
yuenzai <80330775@qq.com>
2025-04-02 18:38:36 +0800

    refactor: 新增 prefect-serve 系统服务

---
+++ install-prefect-serve.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SERVICE_NAME=prefect-serve
+WORKDIR=$(dirname $(readlink -f $0))
+
+sudo tee /etc/systemd/system/${SERVICE_NAME}.service <<EOF
+[Unit]
+Description=${SERVICE_NAME}
+
+[Service]
+Environment="PREFECT_API_URL=http://localhost/prefect/api"
+WorkingDirectory=${WORKDIR}
+ExecStart=${WORKDIR}/prefect/env/bin/python ${WORKDIR}/prefect/kafka_to_s3.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable ${SERVICE_NAME}.service
+
+if [ ! $? -eq 0 ]; then
+  echo "${SERVICE_NAME} install failed"
+  sudo systemctl status ${SERVICE_NAME}.service
+else
+  echo "${SERVICE_NAME} installed"
+fi
