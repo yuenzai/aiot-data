71fd28d6b621e351d5c323e97e8430f138d3d6bf
yuenzai <80330775@qq.com>
2025-04-02 16:39:19 +0800

    refactor(spark): 调整 KafkaToS3Job 任务参数

--- spark/jobs/src/main/java/cn/ecosync/aiot/data/job/KafkaToS3Job.java
+++ spark/jobs/src/main/java/cn/ecosync/aiot/data/job/KafkaToS3Job.java
@@ -6,19 +6,15 @@ import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class KafkaToS3Job {
     private static final Logger log = LoggerFactory.getLogger(KafkaToS3Job.class);
-    private static final DateTimeFormatter FORMATTER_DATETIME = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("([a-zA-Z0-9_-]+\\.){2}[a-zA-Z0-9_-]+");
 
-    private static final String TEMP_KAFKA = "temp_kafka";
-    // 只追加写，因为数据来自 Kafka 属于不可变数据，所以没有 Update 操作
+    private static final String TMP_KAFKA = "tmp_kafka";
     private static final String STATEMENT_MERGE = """
             MERGE INTO %s target
             USING %s source
@@ -27,35 +23,24 @@ public class KafkaToS3Job {
             """;
 
     public static void main(String[] args) {
-        if (args.length != 5) {
-            throw new IllegalArgumentException("topic, timezone, startingTimestamp, endingTimestamp, tableName args are required");
+        log.info("args: {}", Arrays.toString(args));
+        if (args.length < 4) {
+            throw new IllegalArgumentException("args missing, require: [sourceTopic, targetTable, timestamp, offsetHour]");
         }
-        String topic = args[0];
-        String timezone = args[1];
-        String startingDateTime = args[2];
-        String endingDateTime = args[3];
-        String tableName = args[4];
-        Matcher matcher = TABLE_NAME_PATTERN.matcher(tableName);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("Illegal table name: " + tableName);
-        }
+        String sourceTopic = args[0];
+        String targetTable = args[1];
+        long offsetHour = Long.parseLong(args[3]);
+        long endingTimestamp = Long.parseLong(args[2]);
+        long startingTimestamp = endingTimestamp + offsetHour * 60 * 60 * 1000;
 
-        ZoneId zoneId = ZoneId.of(timezone);
-        long startingTimestamp = LocalDateTime.parse(startingDateTime, FORMATTER_DATETIME)
-                .atZone(zoneId)
-                .toInstant()
-                .toEpochMilli();
-        long endingTimestamp = LocalDateTime.parse(endingDateTime, FORMATTER_DATETIME)
-                .atZone(zoneId)
-                .toInstant()
-                .toEpochMilli();
-        log.info("zoneId: {}", zoneId);
-        log.info("startingTimestamp: {}", startingTimestamp);
-        log.info("endingTimestamp: {}", endingTimestamp);
+        Matcher matcher = TABLE_NAME_PATTERN.matcher(targetTable);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Illegal table name: " + targetTable);
+        }
 
         SparkSession spark = SparkSession.builder().appName("Kafka to S3").getOrCreate();
-        extract(spark, topic, startingTimestamp, endingTimestamp);
-        String sql = STATEMENT_MERGE.formatted(tableName, TEMP_KAFKA);
+        extract(spark, sourceTopic, startingTimestamp, endingTimestamp);
+        String sql = STATEMENT_MERGE.formatted(targetTable, TMP_KAFKA);
         spark.sql(sql);
         spark.stop();
     }
@@ -68,6 +53,6 @@ public class KafkaToS3Job {
                 .option("endingTimestamp", String.valueOf(endingTimestamp))
                 .load();
         df = df.selectExpr("CAST(key AS STRING)", "value", "offset", "timestamp");
-        df.createOrReplaceTempView(TEMP_KAFKA);
+        df.createOrReplaceTempView(TMP_KAFKA);
     }
 }
